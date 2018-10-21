35915c1d27d2b3d4336a0533f4156ccbb106b085
sathiyamoorthi <sathiyamoorthiips001@gmail.com>
2018-10-21 15:53:07 +0530

    Adds rental listcomponent, adds rental detail compoenent , add rental service and rental model

---
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { RentalService} from '../shared/rental.service';
+import {Rental} from '../shared/rental.model';
+@Component({
+  selector: 'app-rental-list',
+  templateUrl: './rental-list.component.html',
+  styleUrls: ['./rental-list.component.scss']
+})
+export class RentalListComponent implements OnInit {
+  rentals: Rental[] = [];
+  constructor(private rentalService :RentalService) { }
+
+  ngOnInit() {
+    debugger;
+    const rentalObservable =this.rentalService.getRentals();
+    debugger;
+    rentalObservable.subscribe(
+      (rentals:Rental[])=> {
+        debugger;
+        this.rentals =rentals;
+        //this.rentals[0].
+
+      },
+      (err) =>{
+        debugger;
+
+      },
+      () =>{
+        debugger;
+
+      }); 
+  }
+}
---
+++ src/app/rental/rental.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RentalComponent } from './rental.component';
+import { RentalListComponent } from './rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
+import {RentalDetailComponent} from './rental-detail/rental-detail.component';
+import {CommonModule} from '@angular/common';
+import {RentalService} from './shared/rental.service';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+    { path: 'rentals', component: RentalComponent ,
+    children:[{ path: '', component: RentalListComponent },
+            {path:':rentalId',component:RentalDetailComponent}
+]
+    }
+
+  ];
+@NgModule({
+   
+   
+    declarations: [
+      RentalComponent,
+       RentalListComponent,
+      RentalListItemComponent,
+      RentalDetailComponent
+    ],
+    imports:[CommonModule, RouterModule.forChild(routes)],
+    providers:[RentalService]
+})
+    export class RentalModule { }
+
